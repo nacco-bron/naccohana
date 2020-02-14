1bc218d62e214de2071df5a03d8d4d4ce1b5e18f
nacco-bron <nacco.bron@gmal.com>
2020-02-15 04:33:41 +0900

    発見情報と都道府県市区町村のリレーション設定、発見日時のアクセサ設定

--- src/app/Discovery.php
+++ src/app/Discovery.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-
+use Carbon\Carbon;
 
 class Discovery extends Model
 {
@@ -16,7 +16,34 @@ class Discovery extends Model
         return $this->belongsTo('App\Flower');
     }
 
+    /**
+     * この発見情報に紐づく都道府県を取得
+     */
+    public function prefecture()
+    {
+        return $this->belongsTo('App\Prefecture');
+    }
+    
+    /**
+     * この発見情報に紐づく市区町村を取得
+     */
+    public function city()
+    {
+        return $this->belongsTo('App\City');
+    }
 
+
+    /**
+     * 発見日時アクセサ
+     */
+    function getDiscoveredAtAttribute($value)
+    {
+        return $value ? Carbon::parse($value)->format('Y年m月d日') : null;
+    }
+
+    /**
+     * 緯度経度アクセサ、ミューテタ
+     */
     protected $geofields = array('latlng');
 
     public function setLatlngAttribute(array $value)
